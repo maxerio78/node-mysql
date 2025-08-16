4b39712d817e52a3520940a3db824c16f80189e9
maxerio78 <49488411+maxerio78@users.noreply.github.com>
2025-08-16 09:27:30 +0700

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+// const mysql = require('mysql2');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-});
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+// });
 
-db.getConnection((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL Database!');
-});
+// db.getConnection((err) => {
+//     if (err) throw err;
+//     console.log('Connected to MySQL Database!');
+// });
 
-module.exports = db;
+// module.exports = db;
